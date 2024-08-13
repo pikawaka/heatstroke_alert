96cde3b8611fc941143c496632eba1e4ea261118
toshihiko kobayashi <toshihiko316@gmail.com>
2024-08-13 23:52:43 +0900

    wheneverの設定ファイル作成

---
+++ config/schedule.rb
@@ -0,0 +1,40 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+# Example:
+#
+# set :output, "/path/to/my/cron_log.log"
+#
+# every 2.hours do
+#   command "/usr/bin/some_great_command"
+#   runner "MyModel.some_method"
+#   rake "some:great:rake:task"
+# end
+#
+# every 4.days do
+#   runner "AnotherModel.prune_old_records"
+# end
+
+# Learn more: http://github.com/javan/whenever
+
+# Rails.rootを使用するために必要
+require File.expand_path(File.dirname(__FILE__) + '/environment')
+
+# cronを実行する環境変数
+rails_env = ENV['RAILS_ENV'] || :development
+
+# cronを実行する環境変数をセット
+set :environment, rails_env
+
+# cronのログの吐き出し場所
+set :output, "#{Rails.root}/log/cron.log"
+
+# 30分ごとに実行されるバッチジョブの設定
+# whenever を使ってcronジョブを更新するには、以下のコマンドを実行
+# whenever --update-crontab
+every 30.minutes do
+  # 定期実行したい処理をrunner後にファイル指定
+  # runner "MyBatchJob.perform_now"
+end
