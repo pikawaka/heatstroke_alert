73df23880153b3f634cf9a502982a56ded7412fe
EC2 Default User <ec2-user@ip-172-31-36-68.ap-northeast-1.compute.internal>
2024-08-17 05:48:24 +0000

    web_scraper.rbの追加

---
+++ lib/web_scraper.rb
@@ -0,0 +1,19 @@
+class WebScraper
+  # 村上
+  # 熱中症予防情報サイトの情報をスクレイピングを使って取得
+  def self.get_heatstroke_alert(url)
+    
+  end
+  
+  # 上杉
+  # 取得したWBGTから基準値の範囲を判定
+  def self.judge_wbgt(wbgt)
+    
+  end
+  
+  # 小林
+  # アラートメッセージの通知
+  def self.post_heatstroke_message_notification
+    
+  end
+end
